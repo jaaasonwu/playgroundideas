668932bb4f272fa29c4680bf28ba5d821f69192e
TongNiu <tniu1@student.unimelb.edu.au>
2017-09-05 00:35:33 +1000

    fix the error

---
+++ app/src/main/java/com/playgroundideas/playgroundideas/projects/ProjectsTabPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.playgroundideas.playgroundideas.projects;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.playgroundideas.playgroundideas.R;
+import com.playgroundideas.playgroundideas.projects.ProjectMy;
+import com.playgroundideas.playgroundideas.plans.PlanList;
+
+/**
+ * Created by TongNiu on 4/9/17.
+ */
+
+public class ProjectsTabPagerAdapter extends FragmentPagerAdapter {
+    private Resources resources;
+
+    ProjectsTabPagerAdapter (FragmentManager fm, Context context) {
+        super(fm);
+        this.resources = context.getResources();
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch(i) {
+            case 0: return new ProjectMy();
+            case 1: return new ProjectBrowser();
+            default: return new ProjectMy();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public String getPageTitle(int position) {
+        switch (position) {
+            case 0: return resources.getString(R.string.my_plans);
+            case 1: return resources.getString(R.string.brs_projects);
+            default: return resources.getString(R.string.my_plans);
+        }
+    }
+}
