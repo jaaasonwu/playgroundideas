e80ebc8689f500f1c62ce17e5cf3bda3a1380ccf
Tong Niu <tniu1@student.unimelb.edu.au>
2017-09-05 15:09:01 +1000

    use icon button

--- app/src/main/java/com/playgroundideas/playgroundideas/projects/ProjectMy.java
+++ app/src/main/java/com/playgroundideas/playgroundideas/projects/ProjectMy.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
-import android.widget.ImageButton;
 import com.joanzapata.iconify.widget.IconButton;
 import com.playgroundideas.playgroundideas.MainActivity;
 import com.playgroundideas.playgroundideas.R;
--- app/src/main/java/com/playgroundideas/playgroundideas/projects/ProjectsTabPagerAdapter.java
+++ app/src/main/java/com/playgroundideas/playgroundideas/projects/ProjectsTabPagerAdapter.java
@@ -7,8 +7,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 import com.playgroundideas.playgroundideas.R;
-import com.playgroundideas.playgroundideas.projects.ProjectMy;
-import com.playgroundideas.playgroundideas.plans.PlanList;
 
 /**
  * Created by TongNiu on 4/9/17.
