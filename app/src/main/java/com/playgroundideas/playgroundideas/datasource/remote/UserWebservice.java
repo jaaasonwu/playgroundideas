6bea0f989d0caf317afaf759bbcd8b223422f7a7
Ferdinand Jonathan Swoboda <fswoboda@student.unimelb.edu.au>
2017-09-11 17:56:42 +1000

    introduced remote database layer

---
+++ app/src/main/java/com/playgroundideas/playgroundideas/datasource/remote/UserWebservice.java
@@ -0,0 +1,17 @@
+package com.playgroundideas.playgroundideas.datasource.remote;
+
+import com.playgroundideas.playgroundideas.domain.User;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by Ferdinand on 10/09/2017.
+ */
+
+public interface UserWebservice {
+
+    @GET("/user/{user}")
+    Call<User> getUser(@Path("user") long id);
+}
