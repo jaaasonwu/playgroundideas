6b3d5d657841f7df5a703885f8cd04def09ed021
Ferdinand Jonathan Swoboda <fswoboda@student.unimelb.edu.au>
2017-09-12 16:57:01 +1000

    use Java JSR library to persist currency, persisted address field and design category

--- app/src/main/java/com/playgroundideas/playgroundideas/datasource/local/Database.java
+++ app/src/main/java/com/playgroundideas/playgroundideas/datasource/local/Database.java
@@ -12,7 +12,7 @@ import com.playgroundideas.playgroundideas.domain.User;
  * Created by Ferdinand on 9/09/2017.
  */
 
-@android.arch.persistence.room.Database(entities = {User.class, Plan.class, Design.class, Manual.class, Project.class}, version = 1)
+@android.arch.persistence.room.Database(entities = {User.class, Plan.class, Design.class, Manual.class, Project.class}, version = 1, exportSchema = false)
 public abstract class Database extends RoomDatabase {
     public abstract UserDao userDao();
     public abstract PlanDao planDao();
