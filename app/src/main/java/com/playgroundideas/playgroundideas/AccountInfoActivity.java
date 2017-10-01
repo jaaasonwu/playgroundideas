3d22b7eb6a93cd82dc6476162aaf64804d50eba1
Jiacheng Wu <jiachengw@student.unimelb.edu.au>
2017-10-01 13:24:05 +1100

    Implement the UI for account information page

---
+++ app/src/main/java/com/playgroundideas/playgroundideas/AccountInfoActivity.java
@@ -0,0 +1,17 @@
+package com.playgroundideas.playgroundideas;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jason Wu on 9/30/2017.
+ */
+
+public class AccountInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_info);
+    }
+}
