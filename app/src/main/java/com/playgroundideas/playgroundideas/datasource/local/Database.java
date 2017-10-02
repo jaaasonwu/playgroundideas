02b62494e91b0286a9d0d5356e92da988c1eab2b
Ferdinand Jonathan Swoboda <fswoboda@student.unimelb.edu.au>
2017-10-02 15:03:31 +1100

    added the missing class reference

--- app/src/main/java/com/playgroundideas/playgroundideas/datasource/local/Database.java
+++ app/src/main/java/com/playgroundideas/playgroundideas/datasource/local/Database.java
@@ -3,13 +3,14 @@ package com.playgroundideas.playgroundideas.datasource.local;
 import android.arch.persistence.room.RoomDatabase;
 
 import com.playgroundideas.playgroundideas.model.Design;
+import com.playgroundideas.playgroundideas.model.DesignPictureFileInfo;
 import com.playgroundideas.playgroundideas.model.FavouritedDesignsPerUser;
 import com.playgroundideas.playgroundideas.model.Manual;
 import com.playgroundideas.playgroundideas.model.Project;
 import com.playgroundideas.playgroundideas.model.ProjectPictureFileInfo;
 import com.playgroundideas.playgroundideas.model.User;
 
-@android.arch.persistence.room.Database(entities = {User.class, Design.class, Manual.class, Project.class, FavouritedDesignsPerUser.class, ProjectPictureFileInfo.class}, version = 2)
+@android.arch.persistence.room.Database(entities = {User.class, Design.class, Manual.class, Project.class, FavouritedDesignsPerUser.class, ProjectPictureFileInfo.class, DesignPictureFileInfo.class}, version = 2)
 public abstract class Database extends RoomDatabase {
     public abstract UserDao userDao();
     public abstract ManualDao manualDao();
